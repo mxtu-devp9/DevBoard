f8e919d7cd1e8de6e229bb067233d60c27c563b2
mxtu-devp9 <mitumistry2002@gmail.com>
2026-08-08 09:22:35 +0200

    Add get project by ID endpoint

--- backend/app/api/projects.py
+++ backend/app/api/projects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
@@ -11,6 +11,9 @@ router = APIRouter(
 )
 
 
+# ----------------------------
+# Create Project
+# ----------------------------
 @router.post("/", response_model=ProjectResponse)
 def create_project(
     project: ProjectCreate,
@@ -28,3 +31,33 @@ def create_project(
     db.refresh(new_project)
 
     return new_project
+
+
+# ----------------------------
+# Get All Projects
+# ----------------------------
+@router.get("/", response_model=list[ProjectResponse])
+def get_projects(
+    db: Session = Depends(get_db)
+):
+    projects = db.query(Project).all()
+    return projects
+
+
+# ----------------------------
+# Get Project By ID
+# ----------------------------
+@router.get("/{project_id}", response_model=ProjectResponse)
+def get_project(
+    project_id: int,
+    db: Session = Depends(get_db)
+):
+    project = db.query(Project).filter(Project.id == project_id).first()
+
+    if not project:
+        raise HTTPException(
+            status_code=404,
+            detail="Project not found"
+        )
+
+    return project
